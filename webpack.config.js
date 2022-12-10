77ee04891d3513c76dc074051fbacca0da597ec2
monkey <463149023@qq.com>
2022-12-10 15:38:48 +0800

    1.引入html-loader，处理html中静态资源引用

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,10 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.html$/,
+        use: ['html-loader']
+      },
+      {
         test: /\.css$/,
         use: ['style-loader', 'css-loader']
       },
