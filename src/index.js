7f32b4aacb3a0077df0561c1c05d2579412808d7
monkey <463149023@qq.com>
2022-12-10 13:46:53 +0800

    1.移动首页代码到src目录中 2.使用HtmlWebpackPlugin处理首页html 3.使用style-loader、css-loader处理css 4.配置webpack5 asset资源

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
+import './css/public.css'
+import './css/index.css'
+
 console.log('开始更新架构')
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,39 @@
 const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.js',
+  entry: {
+    bundle: './src/index.js',
+  },
   output: {
-    filename: 'bundle.js',
+    filename: 'js/bundle.js',
     path: path.resolve(__dirname, './dist/')
-  }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        parser: {
+          dataUrlCondition: {
+            maxSize: 4 * 1024
+          }
+        },
+        generator: {
+          filename: 'images/[name].[hash:6][ext]'
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: './src/index.html'
+    })
+  ]
 }
